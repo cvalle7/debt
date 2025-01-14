23dd7a481294cbd253c1807fcfc4f33ec6f91a2f
cvalle7 <cvb16@gmail.com>
2025-01-14 10:15:13 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        hola
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView'
+}
+</script>
+
+<style scoped>
+
+.container{
+    display: flex;
+    align-items: center;
+    background-color: red;
+
+}
+</style>
