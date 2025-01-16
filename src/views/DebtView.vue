6610677652e950285ad55ee96795b84062cec108
cvalle7 <cvb16@gmail.com>
2025-01-16 13:07:50 +0100

    [FEAT] - debt view selector

--- src/views/DebtView.vue
+++ src/views/DebtView.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="container-debt">
         <h1 class="tittle">{{ debt.name }}</h1>
-        <div v-for="(people, index) in debt.people" :key="index" class="user-card">
-            <UserCard :user="people" />
+        <div class="subcontainer" :class="[{ 'hidden': saldosSelector == false }]">
+            <div v-for="(people, index) in debt.people" :key="index" class="user-card">
+                <UserCard :user="people" />
+            </div>
+            <div class="selector">
+                <button :class="[{'selected': saldosSelector == true}]" class="selector-button" @click="changeSaldoSelector()">Saldos</button>
+                <button :class="[{'selected': gastosSelector == true}]" class="selector-button" @click="changeGastosSelector()">Gastos</button>
+            </div>
+        </div>
+        <div class="subcontainer" :class="[{ 'hidden': gastosSelector == false }]">
+            <div class="selector">
+                <button :class="[{'selected': saldosSelector == true}]" class="selector-button" @click="changeSaldoSelector()">Saldos</button>
+                <button :class="[{'selected': gastosSelector == true}]" class="selector-button" @click="changeGastosSelector()">Gastos</button>
+            </div>
         </div>
     </div>
 </template>
@@ -16,7 +28,7 @@ export default {
     methods: {
         getValues() {
 
-            this.debt.people.map(p => {p.pay = 0; p.value = 0})
+            this.debt.people.map(p => { p.pay = 0; p.value = 0 })
 
             for (let pay of this.debt.pays) {
                 let payPart = pay.amount / pay.payFor.length
@@ -26,18 +38,32 @@ export default {
                 userPay.pay += pay.amount;
                 userPay.value += pay.amount;
 
-                for(let pf of pay.payFor){
+                for (let pf of pay.payFor) {
                     const pFor = this.debt.people.filter(p => p.id === pf);
                     let userPayFor = pFor[0];
                     userPayFor.value -= payPart
                 }
             }
+        },
+        changeSaldoSelector() {
+            this.gastosSelector = false;
+            this.saldosSelector = true;
+        },
+        changeGastosSelector(){
+            this.gastosSelector = true;
+            this.saldosSelector = false;
         }
     },
     computed: {
         debt() {
             const debtStore = useStoreDebt();
             return debtStore.debt;
+        }
+    },
+    data() {
+        return {
+            gastosSelector: false,
+            saldosSelector: true
         }
     },
     mounted() {
@@ -62,12 +88,60 @@ export default {
     color: white;
 }
 
-.user-card {
-    width: 30%;
-    margin-bottom: 1%;
+.subcontainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    border-radius: 15px;
+    background-color: rgb(77, 71, 71);
+    width: 50%;
 }
 
-.tittle{
-    margin: 4% 0;
+.user-card {
+    margin-top: 2%;
+    width: 60%;
+}
+
+.tittle {
+    margin: 4% 0 3% 0;
+}
+
+.hidden {
+    display: none;
+}
+
+.selector {
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2% 0;
+    border: 2px solid #EFEFEF;
+    border-radius: 5px;
+}
+
+.selector-button{
+    color: white;
+    background-color: #1B1818;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 5vh;
+    font-size: large;
+    transition: background-color 0.5s ease;
+}
+
+.selector-button:hover{
+    background-color: #EFEFEF;
+    cursor: pointer;
+    color: black;
+}
+
+.selected{
+    background-color: #EFEFEF;
+    color: black;
 }
 </style>
