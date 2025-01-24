0e9ed5d6674177106bb8350e9608fb1f5309d690
cvalle7 <cvb16@gmail.com>
2025-01-24 14:19:39 +0100

    [FEAT] - init debt service

--- debt-backend/src/debt/debt.controller.ts
+++ debt-backend/src/debt/debt.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DebtService } from './debt.service';
 
 @Controller('debt')
-export class DebtController {}
+export class DebtController {
+
+    constructor(private readonly debtService: DebtService){}
+
+    @Get()
+    async getDebts(){
+
+    }
+}
