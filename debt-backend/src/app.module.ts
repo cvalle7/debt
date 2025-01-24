98b33e526f194d923b93c32b9822834fc7495e64
cvalle7 <cvb16@gmail.com>
2025-01-24 12:58:01 +0100

    [FEAT] - backend added

--- debt-backend/src/app.module.ts
+++ debt-backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.BD_HOST,
+      port: parseInt(process.env.BD_PORT),
+      username: process.env.BD_USERNAME,
+      password: process.env.BD_PASSWORD,
+      database: process.env.BD_DATABASE,
+      entities: [User],
+      synchronize: true
+    }),
+    UserModule
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
