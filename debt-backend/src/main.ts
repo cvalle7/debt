a6d287a2c698f9699a27434e6a19e99b2e770e61
cvalle7 <cvb16@gmail.com>
2025-01-29 14:00:10 +0100

    [FEAT] - cors added

--- debt-backend/src/main.ts
+++ debt-backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE, PATCH',
+    allowedHeaders: 'Content-Type, Accept'
+  });
   await app.listen(parseInt(process.env.APP_PORT));
 }
 bootstrap();
